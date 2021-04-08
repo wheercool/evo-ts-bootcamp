3e313ebd25a26592d000a0c11b163d77cfb0a161
MINSK\wheercool <A.Rudy@sam-solutions.com>
2021-04-08 19:10:52 +0300

    Initial commint

---
+++ homeworks/01-bubble-sort/README.md
@@ -0,0 +1,2 @@
+ # Bubble Sort Visualization
+ Solution for [issue](https://github.com/evolution-gaming/typescript-bootcamp/tree/main/homeworks/01-bubble-sort)
---
+++ homeworks/01-bubble-sort/src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        Bubble Sort Visualization
+      </header>
+    </div>
+  );
+}
+
+export default App;
